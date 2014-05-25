a8c1f93d68a261d83a08b1cbd2afe80d7d5b4cef
leeed1998 <ed@nordlee.com>
2014-05-24 22:34:34 -0400

    Ed Assignment 2 R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Edward Lee - 5/24/2014
+# R Programming Assignment 2
+# CacheMatrix - and Get Inverse of the Matrix for caching
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	#Defines/Resets the cache m
+	InverseMatrix <- NULL   
+	        set <- function(y) {
+                x <<- y
+                InverseMatrix <<- NULL
+        }
+
+	#Get the matrix
+	get <- function () x
+	# now get the inverse of the matrix
+	setInverse <- function(I) InverseMatrix <<- I
+	getInverse <- function() InverseMatrix 
+	# get the inverse now
+	list (set = set, get = get, 
+		setInverse = setInverse,
+		getInverse  = getInverse )
 
 }
 
@@ -12,4 +28,33 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		      #Invert the matrix
+        InverseMatrix <- x$getInverse ()
+      # Look for previous matrix
+        if(!is.null(InverseMatrix)) {
+                return(InverseMatrix)
+        }
+      # if not: get and solve the inverse of the matrix.
+        cm <- x$get()
+        InverseMatrix <- solve(cm, ...)
+      # set the inverse of the matrix.
+        x$setInverse (InverseMatrix)
+        InverseMatrix
+
 }
+
+##Test and Use - got this from the Course Forum - Test cases Please!
+
+
+amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+amatrix$get()         # Returns original matrix
+cacheSolve(amatrix)   # Computes, caches, and returns    matrix inverse
+amatrix$getInverse()  # Returns matrix inverse
+cacheSolve(amatrix)   # Returns cached matrix inverse using previously computed matrix inverse
+
+amatrix$set(matrix(c(0,5,99,66), nrow=2, ncol=2)) # Modify existing matrix
+cacheSolve(amatrix)   # Computes, caches, and returns new matrix inverse
+amatrix$get()         # Returns matrix
+amatrix$getInverse()  # Returns matrix inverse
+
+
